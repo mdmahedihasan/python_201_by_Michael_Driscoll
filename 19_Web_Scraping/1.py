bdf52b895fc1ff897686647c7cfb33692798fa81
mahedi <mahedi_h@yahoo.com>
2017-05-08 05:03:37 +0600

    finish 19

---
+++ 19_Web_Scraping/1.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://twitter.com/mousevspython"
+
+req = requests.get(url)
+html = req.text
+soup = BeautifulSoup(html, "html.parser")
+tweets = soup.find_all("li", "js-stream-item")
+
+for item in range(len(soup.find_all("p", "TweetTextSize"))):
+    tweet_text =tweets[item].get_text()
+    print(tweet_text)
+    dt = tweets[item].find("a", "tweet-timestamp")
+    print("this was tweeted on " + dt)
---
+++ 19_Web_Scraping/test1.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "http://www.blog.pythonlibrary.org/"
+
+
+def get_articles():
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html, "html.parser")
+    pages = soup.find_all("h1")
+
+    articles = {i.a["href"]: i.text.strip()
+                for i in pages if i.a}
+
+    for article in articles:
+        s = "{title}: {url}".format(
+            title=articles[article],
+            url=article
+        )
+        print(s)
+    return articles
+
+
+if __name__ == '__main__':
+    articles = get_articles()
